fa86af426a8d4e6b09e5e6ab78f06767509b17ff
Newwissam <mmdhkwr85@gmail.com>
2024-05-29 20:56:01 +0300

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,139 @@
+HELP_1 = """<b>• اوامر المشرفين •
+
+ - اوامر المكالمة :
+ 
+ ايقاف مؤقت - لايقاف التشغيل مؤقت في المكالمة
+ 
+ اسئناف - لاستئناف التشغيل 
+ 
+ ايقاف - لايقاف التشغيل في المكالمة
+ 
+ تخطي - لتخطي المُشغل في المكالمة وتشغيل المسار التالي
+ 
+ تكرار + العدد - لتكرار المشغل في المكالمة
+ 
+ السرعة - لزيادة او بطئ سرعة المُشغل في المكالمة
+
+
+ </b>
+"""
+
+HELP_2 = """
+<b>• اوامر التشغيل في المكالمة •
+
+شغلي او شغل او /play  + اسم الاغنية او الصوت المطلوب 
+وأيضا تقدر تشغل بالرد على الملف الصوتي او تقدر تشغل قائمة الحفط الخاصة بك ( اذا كان عندك )
+</b>
+"""
+
+HELP_3 = """<b>"اهلين فيك في اوامر البحث ♪
+للبحث عن اغنية او فيديو استخدم الامر التالي ↓
+
+[ بحث + اسم المطلوب ..]
+
+مثال -› بحث بحبك وحشتني
+
+- الامر يشتغل بخاص البوت والمجموعة ايضا ..</b>"""
+
+HELP_4 = """<b>✧ اهلين فيك في قسم حفظ التشغيل\n\n- حفظ التشغيل هو حفظ الاغاني الي اشتغلت بالمجموعة وحفظها يعني انك تقدر تشغلها بدون ما ترجع تبحث عنها مرة ثانية وتبقى محفوظة لك فقط**\n\n- عشان تحفظ الاغنية او المُشغل الحالي بالمكالمة لازم تضغط على زر -› ( حفظ التشغيل )\n\n- عشان تشوف الاغاني او الصوتيات الي حفظتها اكتب امر -› ( قائمة تشغيلي )\n\n- وطريقة تشغيل قائمتك تكتب فقط امر -› ( تشغيل قائمتي )\n\n- طريقة حذف اغنية او مقطع من محفوظاتك تكتب امر -› ( حذف التشغيل ) وتكمل الخطوات بخاص البوت ..\n\n✶ ملاحظة : اذا حفظت اغنية بتكون محفوظة عندك فقط يعني كل شخص عنده قائمة تشغيل خاصة فيه ومحد يقدر يحفظ اغنية عندك والعكس ايضا\n✶ لو ما فهمت تابع الفيديو الي فوق عشان تفهم اكثر ❤️</b>
+"""
+
+HELP_5 = """
+<u><b>block users:</b></u> [only for sudoers]
+
+starts ignoring the blacklisted user, so that he can't use bot commands.
+
+/block [username or reᴩly to a user] : block the user from our bot.
+/unblock [username or reᴩly to a user] : unblocks the blocked user.
+/blockedusers : shows the list of blocked users.
+"""
+
+HELP_6 = """
+<u><b>channel ᴩlay commands:</b></u>
+
+you can stream audio/video in channel.
+
+/cplay : starts streaming the reouested audio track on channel's videochat.
+/cvplay : starts streaming the reouested video track on channel's videochat.
+/cplayforce or /cvplayforce : stoᴩs the ongoing stream and starts streaming the reouested track.
+
+/channelplay [chat username or id] or [disable] : connect channel to a grouᴩ and starts streaming tracks by the helᴩ of commands sent in grouᴩ.
+"""
+
+HELP_7 = """
+<u><b>global ban feature</b></u> [only for sudoers] :
+
+/gban [username or reᴩly to a user] : globally bans the chutiya from all the served chats and blacklist him from using the bot.
+/ungban [username or reᴩly to a user] : globally unbans the globally banned user.
+/gbannedusers : shows the list of globally banned users.
+"""
+
+HELP_8 = """
+<b><u>loop stream :</b></u>
+
+<b>starts streaming the ongoing stream in loop</b>
+
+/loop [enable/disable] : enables/disables loop for the ongoing stream
+/loop [1, 2, 3, ...] : enables the loop for the given value.
+"""
+
+HELP_9 = """
+<u><b>maintenance mode</b></u> [only for sudoers] :
+
+/logs : get logs of the bot.
+
+/logger [enable/disable] : bot will start logging the activities haᴩᴩen on bot.
+
+/maintenance [enable/disable] : enable or disable the maintenance mode of your bot.
+"""
+
+HELP_10 = """
+<b><u>ping & stats :</b></u>
+
+/start : starts the music bot.
+/help : get helᴩ menu with exᴩlanation of commands.
+
+/ping : shows the ᴩing and system stats of the bot.
+
+/stats : shows the overall stats of the bot.
+"""
+
+HELP_11 = """
+<u><b>ᴩlay commands :</b></u>
+
+<b>v :</b> stands for video ᴩlay.
+<b>force :</b> stands for force ᴩlay.
+
+/play or /vplay : starts streaming the reouested track on videochat.
+
+/playforce or /vplayforce : stoᴩs the ongoing stream and starts streaming the reouested track.
+"""
+
+HELP_12 = """
+<b><u>shuffle oueue :</b></u>
+
+/shuffle : shuffle's the oueue.
+/queue : shows the shuffled oueue.
+"""
+
+HELP_13 = """
+<b><u>seek stream :</b></u>
+
+/seek [duration in seconds] : seek the stream to the given duration.
+/seekback [duration in seconds] : backward seek the stream to the the given duration.
+"""
+
+HELP_14 = """
+<b><u>song download</b></u>
+
+/song [song name/yt url] : download any track from youtube in mp3 or mp4 formats.
+"""
+
+HELP_15 = """
+<b><u>speed commands :</b></u>
+
+you can control the playback speed of the ongoing stream. [admins only]
+
+/speed or /playback : for adjusting the audio playback speed in group.
+/cspeed or /cplayback : for adjusting the audio playback speed in channel.
+"""
